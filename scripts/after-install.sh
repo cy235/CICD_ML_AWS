cfccb9335ecd7af00d7efa9edac5e657e60032c2
cy235 <44735485+cy235@users.noreply.github.com>
2020-02-05 12:33:59 -0500

    Update after-install.sh

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/cy235-app
 source .venv/bin/activate
-sudo apt install python3-pip -y
-sudo pip3 install -r requirements.txt
+# sudo apt install python3-pip -y
+# sudo pip3 install -r requirements.txt
 sudo sh run_docker.sh
